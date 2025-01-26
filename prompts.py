d8a5e38a7b37d7d33530b70a11d74bd1f252a291
Samuel Eras <sa.eras@gmx.de>
2025-01-26 20:46:45 +0000

    create prompts python file for prompt assembly

---
+++ prompts.py
@@ -0,0 +1,29 @@
+import json
+import os
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+
+def load_prompts(file_path: str):
+    with open(file_path, "r") as file:
+        return json.load(file)
+
+
+app_path = os.path.dirname(os.path.abspath(__file__))
+prompts = load_prompts(os.path.join(app_path, "prompts.json"))
+
+
+def queryPromptwithContext():
+    return ChatPromptTemplate(
+        [
+            (
+                "system",
+                prompts["queryPromptwithContext"]["default_prompt"],
+            ),
+            MessagesPlaceholder("chat_history"),
+            MessagesPlaceholder("input"),
+            (
+                "system",
+                prompts["queryPromptwithContext"]["followup_prompt"],
+            ),
+        ]
+    )
