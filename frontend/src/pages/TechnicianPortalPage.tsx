b3263c8d42065e9c69167497e7741635e40aa5f8
Samuel Eras <sa.eras@gmx.de>
2025-02-18 01:28:21 +0000

    feat: add route protection for technician-portal

--- frontend/src/pages/TechnicianPortalPage.tsx
+++ frontend/src/pages/TechnicianPortalPage.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import useAuthStore from "../stores/AuthStore";
+import { useNavigate } from "react-router-dom";
+
 const TechnicianPortalPage = () => {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user || Object.keys(user).length === 0) return; //This case does not need to be handled here and might interfere on reload
+    if (user?.group !== "technicians") {
+      console.log("Access restricted!");
+      navigate(-1); //return to previous page
+    }
+  }, [user]);
+
   return (
     <div>
       <h1>TechnicianPortalPage</h1>
