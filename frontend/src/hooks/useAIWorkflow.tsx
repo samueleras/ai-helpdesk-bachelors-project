0860d69246bbe86783f2f3aa4bf923f8600bcb92
Samuel Eras <sa.eras@gmx.de>
2025-03-03 15:51:02 +0000

    feat: add useAIWorkflow hook for AI interaction

---
+++ frontend/src/hooks/useAIWorkflow.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/apiClient";
+import { WorkflowResponse } from "../entities/WorkflowRepsonse";
+import { WorkflowRequest } from "../entities/WorkflowRequest";
+
+const workflowClient = new APIClient<WorkflowResponse>("/init_ai_workflow");
+
+const useAIWorkflow = (
+  workflowRequest: WorkflowRequest,
+  accessToken: string
+) => {
+  return useQuery<WorkflowResponse>({
+    queryKey: ["workflow", workflowRequest],
+    queryFn: () => workflowClient.initAIWorkflow(workflowRequest, accessToken),
+    retry: 2,
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    refetchInterval: false,
+  });
+};
+
+export default useAIWorkflow;
