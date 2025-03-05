4bc014417ff5fe6afb32e87d2d6732277fda508a
Samuel Eras <sa.eras@gmx.de>
2025-03-05 14:39:07 +0000

    feat: implement navbar for desktop

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import { NavLink } from "react-router";
 import { useMsal } from "@azure/msal-react";
+import { Box, Button, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useAuthStore from "../stores/AuthStore";
+import StyledNavLink from "./StyledNavLink";
+import { TbLogout } from "react-icons/tb";
 
 export function Navbar() {
   const { instance } = useMsal();
   const { logout } = useAuthStore();
+  const navigate = useNavigate();
 
   const azurelogout = () => {
     instance.clearCache();
@@ -13,18 +18,51 @@ export function Navbar() {
   };
 
   return (
-    <nav>
-      <br />
-      <NavLink to="/ai-chat" end>
-        AI Chat
-      </NavLink>
-      <br />
-      <NavLink to="/technician-portal">Technician Portal</NavLink> <br />
-      <NavLink to="/my-tickets">My Tickets</NavLink>
-      <br />
-      <NavLink to="/" onClick={azurelogout}>
-        Logout
-      </NavLink>
-    </nav>
+    <Flex
+      px={6}
+      py={4}
+      boxShadow="md"
+      position="sticky"
+      top={0}
+      zIndex={1000}
+      h={"4rem"}
+    >
+      <Flex
+        justify="space-between"
+        align="center"
+        alignContent={"center"}
+        w={"100vw"}
+      >
+        <Box style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+          AI Helpdesk
+        </Box>
+
+        {/* Desktop Navbar */}
+        <Flex display={{ base: "none", md: "flex" }} gap={6}>
+          <StyledNavLink link="/ai-chat" name="AI Chat" bottomline={true} />
+          <StyledNavLink
+            link="/technician-portal"
+            name="Technician Portal"
+            bottomline={true}
+          />
+          <StyledNavLink
+            link="/my-tickets"
+            name="My Tickets"
+            bottomline={true}
+          />
+        </Flex>
+
+        <Button
+          display={{ base: "none", md: "flex" }}
+          onClick={() => {
+            azurelogout();
+            navigate("/");
+          }}
+        >
+          Logout
+          <TbLogout />
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
