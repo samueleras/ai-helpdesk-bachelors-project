5aac6358da4be4ca7fc1aa9b9629749113b7ba98
Samuel Eras <sa.eras@gmx.de>
2025-03-05 14:39:31 +0000

    feat: implement navbar for mobile

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,24 @@
+import {
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerRoot,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { useMsal } from "@azure/msal-react";
-import { Box, Button, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../stores/AuthStore";
 import StyledNavLink from "./StyledNavLink";
 import { TbLogout } from "react-icons/tb";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export function Navbar() {
   const { instance } = useMsal();
   const { logout } = useAuthStore();
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
   const azurelogout = () => {
@@ -62,6 +72,63 @@ export function Navbar() {
           Logout
           <TbLogout />
         </Button>
+
+        {/* Mobile Navbar */}
+        <DrawerRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
+          <DrawerBackdrop />
+          <DrawerTrigger asChild>
+            <Button
+              variant="outline"
+              size="sm"
+              display={{ base: "flex", md: "none" }}
+            >
+              <GiHamburgerMenu />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerCloseTrigger />
+            <DrawerBody>
+              <Flex
+                justifyContent={"space-between"}
+                h={"100%"}
+                paddingTop={10}
+                paddingBottom={5}
+                flexDirection={"column"}
+              >
+                <VStack gap="5" alignItems={"start"}>
+                  <StyledNavLink
+                    link="/ai-chat"
+                    name="AI Chat"
+                    sideline={true}
+                    callBack={setOpen}
+                  />
+                  <StyledNavLink
+                    link="/technician-portal"
+                    name="Technician Portal"
+                    sideline={true}
+                    callBack={setOpen}
+                  />
+                  <StyledNavLink
+                    link="/my-tickets"
+                    name="My Tickets"
+                    sideline={true}
+                    callBack={setOpen}
+                  />
+                </VStack>
+                <Button
+                  onClick={() => {
+                    azurelogout();
+                    setOpen(false);
+                    navigate("/");
+                  }}
+                >
+                  <TbLogout />
+                  Logout
+                </Button>
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerRoot>
       </Flex>
     </Flex>
   );
