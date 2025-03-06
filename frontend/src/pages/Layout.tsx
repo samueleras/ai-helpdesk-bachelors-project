c0d590630211af39703a9c1f94acd6ff225cd432
Samuel Eras <sa.eras@gmx.de>
2025-03-06 21:58:51 +0000

    fix: wait for authentication before rendering components

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import useAuthStore from "@/stores/AuthStore";
 
 const Layout = () => {
+  const { accessToken } = useAuthStore();
+  if (!accessToken) {
+    return;
+  }
   return (
     <>
       {<Navbar />}
