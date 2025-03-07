146e1e6446486eaac523756037d83d243bf0131f
Samuel Eras <sa.eras@gmx.de>
2025-03-07 00:02:06 +0000

    feat: add Ticket entity interface

---
+++ frontend/src/entities/Ticket.ts
@@ -0,0 +1,23 @@
+export interface SimilarTicket {
+  ticket_id: number;
+  title: string;
+}
+
+export interface TicketMessage {
+  message: string;
+  author_name: string;
+  group: string;
+  created_at: Date;
+}
+
+export interface Ticket {
+  ticket_id: number;
+  title: string;
+  content: string;
+  creation_date: Date;
+  closed_date?: Date | null;
+  author_name: string;
+  assignee_name?: string | null;
+  similar_tickets: SimilarTicket[];
+  ticket_messages: TicketMessage[];
+}
