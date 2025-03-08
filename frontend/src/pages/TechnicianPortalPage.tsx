32a8ed0420559c5e5bd8e1f2dc0ca207f68eaf96
Samuel Eras <sa.eras@gmx.de>
2025-03-08 11:13:29 +0000

    feat: implement ticket fetching and unstyled display for TechnicianPortalPage

--- frontend/src/pages/TechnicianPortalPage.tsx
+++ frontend/src/pages/TechnicianPortalPage.tsx
@@ -1,23 +1,34 @@
 import { useEffect } from "react";
 import useAuthStore from "../stores/AuthStore";
 import { useNavigate } from "react-router-dom";
+import { Spinner, Text } from "@chakra-ui/react";
+import useFilteredTickets from "@/hooks/useFilteredTickets";
 
 const TechnicianPortalPage = () => {
-  const { user } = useAuthStore();
+  const { user, accessToken } = useAuthStore();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user || Object.keys(user).length === 0) return; //This case does not need to be handled here and might interfere on reload
     if (user?.group !== "technicians") {
       console.log("Access restricted!");
       navigate(-1); //return to previous page
     }
   }, [user]);
 
+  const {
+    data: tickets,
+    error,
+    isFetching,
+  } = useFilteredTickets(accessToken, { page: 1, page_size: 10 });
+
   return (
     <div>
-      <h1>TechnicianPortalPage</h1>
-      <p>content</p>
+      <h1>MyTicketsPage</h1>
+      {error && "Error"}
+      {isFetching && <Spinner />}
+      {tickets?.map((ticket) => (
+        <Text key={ticket.ticket_id}>{ticket.title}</Text>
+      ))}
     </div>
   );
 };
