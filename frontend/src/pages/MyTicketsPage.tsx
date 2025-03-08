61c697ae255d4c3224514ec2fa2f2de7b54ea844
Samuel Eras <sa.eras@gmx.de>
2025-03-08 11:13:37 +0000

    feat: implement ticket fetching and unstyled display for MyTicketsPage

--- frontend/src/pages/MyTicketsPage.tsx
+++ frontend/src/pages/MyTicketsPage.tsx
@@ -1,8 +1,23 @@
+import useMyTickets from "@/hooks/useMyTickets";
+import useAuthStore from "@/stores/AuthStore";
+import { Spinner, Text } from "@chakra-ui/react";
+
 const MyTicketsPage = () => {
+  const { accessToken } = useAuthStore();
+  const {
+    data: tickets,
+    error,
+    isFetching,
+  } = useMyTickets(accessToken, { page: 1, page_size: 10 });
+
   return (
     <div>
       <h1>MyTicketsPage</h1>
-      <p>content</p>
+      {error && "Error"}
+      {isFetching && <Spinner />}
+      {tickets?.map((ticket) => (
+        <Text key={ticket.ticket_id}>{ticket.title}</Text>
+      ))}
     </div>
   );
 };
