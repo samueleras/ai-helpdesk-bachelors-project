c8b6c225a21500760772606243df62b98824928e
Samuel Eras <sa.eras@gmx.de>
2025-03-08 11:07:51 +0000

    feat: update filter entity interfaces on frontend

--- frontend/src/entities/Filter.ts
+++ frontend/src/entities/Filter.ts
@@ -1,5 +1,10 @@
-export interface Filter {}
+export interface Filter {
+  page?: number;
+  page_size?: number;
+}
 
 export interface TicketFilter extends Filter {
   assignee_id?: string;
+  closed?: boolean;
+  author_id?: string;
 }
