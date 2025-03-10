4f8b752206e9ff2b98c6ddf5d09b820237f07162
Samuel Eras <sa.eras@gmx.de>
2025-03-10 15:46:09 +0000

    feat: implement ticket assignment in AssignDropdown component

--- frontend/src/components/AssignDropdown.tsx
+++ frontend/src/components/AssignDropdown.tsx
@@ -5,11 +5,10 @@ import {
   MenuTrigger,
 } from "@/components/ui/menu";
 import { Ticket } from "@/entities/Ticket";
+import useAssignTicket from "@/hooks/useAssignTicket";
 import useTechnicians from "@/hooks/useTechnicians";
 import useAuthStore from "@/stores/AuthStore";
 import { Button, MenuItemGroup } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-
 interface AssignDropdownProps {
   ticket: Ticket;
 }
@@ -17,25 +16,33 @@ interface AssignDropdownProps {
 export const AssignDropdown = ({ ticket }: AssignDropdownProps) => {
   const { accessToken } = useAuthStore();
   const { data: technicians, error } = useTechnicians(accessToken);
-  const [assignee, setAssignee] = useState(
-    ticket.assignee_name || "Unassigned"
-  );
+  const { mutate: assignTicket } = useAssignTicket();
 
-  useEffect(() => {
-    console.log("change assignee", assignee);
-  }, [assignee]);
+  const handleClick = (assignee: string) => {
+    const assigneeObj = technicians?.find(
+      (technician) => technician.user_name == assignee
+    );
+    ticket.assignee_name = assignee;
+    assignTicket({
+      accessToken,
+      ticketAssignee: {
+        ticket_id: ticket.ticket_id,
+        assignee_id: assigneeObj?.user_id || "Unassigned",
+      },
+    });
+  };
 
   return (
     <MenuRoot>
       <MenuTrigger asChild h="1.3rem">
         <Button variant="outline" size="sm">
-          {assignee}
+          {ticket.assignee_name || "Unassigned"}
         </Button>
       </MenuTrigger>
       <MenuContent>
         <MenuItemGroup>
           <MenuItem
-            onClick={() => setAssignee("Unassigned")}
+            onClick={() => handleClick("Unassigned")}
             value="Unassigned"
           >
             {error ? "Failed to load Technicians" : "Unassigned"}
@@ -43,7 +50,7 @@ export const AssignDropdown = ({ ticket }: AssignDropdownProps) => {
           {technicians?.map((technician) => (
             <MenuItem
               value={technician.user_name}
-              onClick={() => setAssignee(technician.user_name)}
+              onClick={() => handleClick(technician.user_name)}
               key={technician.user_id}
             >
               {technician.user_name}
