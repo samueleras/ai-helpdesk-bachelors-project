d963fc1111d5e16904ca24d75757021a07f494c3
Samuel Eras <sa.eras@gmx.de>
2025-03-13 19:53:07 +0000

    fix: redirect to LoginPage if user is unauthenticated or lacks required security groups

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import useAuthStore from "@/stores/AuthStore";
 import { Box } from "@chakra-ui/react";
 
 const Layout = () => {
-  const { accessToken } = useAuthStore();
-  if (!accessToken) {
+  const { accessToken, user } = useAuthStore();
+  const navigate = useNavigate();
+  if (!accessToken || user.group == "") {
+    navigate("/");
     return;
   }
   return (
