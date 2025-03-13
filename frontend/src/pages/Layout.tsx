de2e59997e69673f9b7b8826bdc9e2dc468596f4
Samuel Eras <sa.eras@gmx.de>
2025-03-13 22:44:00 +0000

    fix: navigate to login page for unauthenticated users using useEffect

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -2,12 +2,19 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import useAuthStore from "@/stores/AuthStore";
 import { Box } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 const Layout = () => {
   const { accessToken, user } = useAuthStore();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!accessToken || user.group == "") {
+      navigate("/");
+    }
+  }, []);
+
   if (!accessToken || user.group == "") {
-    navigate("/");
     return;
   }
   return (
