2df615d92fc0a2769d1146103f7bfbe57fe293f1
Samuel Eras <sa.eras@gmx.de>
2025-03-17 18:40:13 +0000

    test: Add unit test for /api/close-ticket endpoint

---
+++ tests/test_close_ticket.py
@@ -0,0 +1,44 @@
+import os
+from fastapi.testclient import TestClient
+from backend.main import app
+
+client = TestClient(app)
+
+AZURE_TECHNICIAN_TESTING_ACCESS_TOKEN = os.getenv(
+    "AZURE_TECHNICIAN_TESTING_ACCESS_TOKEN"
+)
+assert (
+    AZURE_TECHNICIAN_TESTING_ACCESS_TOKEN
+), "AZURE_TECHNICIAN_TESTING_ACCESS_TOKEN is not set in environment variables!"
+AZURE_USER_TESTING_ACCESS_TOKEN = os.getenv("AZURE_USER_TESTING_ACCESS_TOKEN")
+assert (
+    AZURE_USER_TESTING_ACCESS_TOKEN
+), "AZURE_USER_TESTING_ACCESS_TOKEN is not set in environment variables!"
+VALID_TICKET_ID = os.getenv("VALID_TICKET_ID")
+assert VALID_TICKET_ID, "VALID_TICKET_ID is not set in environment variables!"
+
+
+def test_close_ticket_authorized():
+
+    request_data = {"ticket_id": int(VALID_TICKET_ID)}
+
+    response = client.post(
+        "/api/close-ticket",
+        headers={"Authorization": f"Bearer {AZURE_TECHNICIAN_TESTING_ACCESS_TOKEN}"},
+        json=request_data,
+    )
+
+    assert response.status_code == 200, f"Unexpected response: {response.json()}"
+
+
+def test_close_ticket_unauthorized():
+
+    request_data = {"ticket_id": int(VALID_TICKET_ID)}
+
+    response = client.post(
+        "/api/close-ticket",
+        headers={"Authorization": f"Bearer {AZURE_USER_TESTING_ACCESS_TOKEN}"},
+        json=request_data,
+    )
+
+    assert response.status_code == 403, f"Unexpected response: {response.json()}"
